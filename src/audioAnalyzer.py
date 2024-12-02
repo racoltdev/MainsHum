f00c5d42da951c39e0057af05c7b88d1dab544d2
racoltdev <racoltdev@gmail.com>
2024-12-02 12:50:05 -0500

    functioning downsampling

--- src/audioAnalyzer.py
+++ src/audioAnalyzer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.signal as sig
+import math
 
 import audioConverter
 
@@ -15,12 +16,44 @@ def freqExtract(sample, freq_min, freq_max):
 	#low_cut = audioConverter.HzToRads(freq_min)
 	#high_cut = audioConverter.HzToRads(freq_max)
 
+	data, sample_rate = down_sample(data, sample_rate, freq_min, freq_max)
 	# Fall off rate of frequencies outside the range
 	order = 5
 	bandpass_filter = lambda d : butter_bandpass(d, freq_min, freq_max, sample_rate, order)
 	pass_through = np.apply_along_axis(bandpass_filter, 0, data).astype('int16')
 	output = normalize(pass_through)
 	return output, sample_rate
+
+
+def down_sample(data, sample_rate, freq_min, freq_max):
+	band = freq_max - freq_min
+
+	if band > (0.005 * sample_rate):
+		print("\tDown sampling not needed. Band is wide enough")
+		return data, sample_rate
+
+	# Yeah, this is magic, but it's just a random eq to get a roughly ok sampling rate
+	# Desired is at least 1 order of magnitude greater than freq_max
+	desired_rate = int(10 ** math.ceil(np.log10(freq_max)) * 1.5)
+
+	factor = int(sample_rate / desired_rate)
+
+	# Final sample rate will not be exactly the desired rate because the factor must be an int
+	print(f"\tDownsampling from {sample_rate}Hz to ~{desired_rate}Hz")
+	# This sucks
+	# decimate should only be done with a factor of 13 max at a time
+	# this is the best I could come up with to step with decimate
+	while factor >= 10:
+		factor /= 10
+		sample_rate /= 10
+		data = sig.decimate(data, 10, axis=0)
+
+	# Must keep type cast of factor and sample_rate in sync
+	factor = int(factor)
+	if factor > 1:
+		sample_rate = int(sample_rate / factor)
+		data = sig.decimate(data, factor, axis=0)
+	return data, int(sample_rate)
 
 
 def butter_bandpass(data, low_cut, high_cut, sample_rate, order):
